f1a9b92ed97ffb165ecc0531e08d4f1a1746a629
Paul Badalian <bad.alien7@gmail.com>
2015-12-15 18:11:05 -0500

    Added src files

---
+++ server.py
@@ -0,0 +1,53 @@
+import socket
+import sys
+
+# Create a socket
+def socket_create():
+    try:
+        global host
+        global port
+        global s
+        host = ""
+        port = 9999
+        s = socket.socket()
+    except socket.error as msg:
+        print("Socket creation error: " + str(msg))
+
+# Bind socket to port and wait for connection from client
+def socket_bind():
+    try:
+        global host
+        global port
+        global s
+        print("Binding socket to port: " + str(port))
+        s.bind((host, port))
+        s.listen(5)
+    except socket.error as msg:
+        print("Socket binding error: " + str(msg) + "\n" + "Retrying...")
+
+# Establish a connection with client
+def socket_accept():
+    conn, address = s.accept()
+    print("Connection has been established | " + "IP" + address[0] + " | Port " + str(address[1]))
+    send_commands(conn)
+    conn.close()
+
+def send_commands(conn):
+    while True:
+        cmd = input()
+        if cmd == 'quit':
+            conn.close()
+            s.close()
+            sys.exit()
+        elif len(str.encode(cmd)) > 0:
+            conn.send(str.encode(cmd))
+            client_response = str(conn.recv(1024), "utf-8")
+            print(client_response, end="")
+
+def main():
+    socket_create()
+    socket_bind()
+    socket_accept()
+
+if __name__ == "__main__":
+    main()
