237a3061b8a28a02aaf010b4235ab5c575c6d297
Paul Badalian <Paul Badalian>
2015-12-21 17:08:40 -0800

    Edited server.py file from windows pc

--- server.py
+++ server.py
@@ -1,5 +1,14 @@
 import socket
 import sys
+import time
+import threading
+from queue import Queue
+
+NUMBER_OF_THREADS = 2
+JOB_NUMBER = [1, 2]
+queue = Queue()
+all_connections = []
+all_addresses = []
 
 # Create a socket
 def socket_create():
@@ -24,30 +33,25 @@ def socket_bind():
         s.listen(5)
     except socket.error as msg:
         print("Socket binding error: " + str(msg) + "\n" + "Retrying...")
+        time.sleep(5)
+        socket_bind()
 
 # Establish a connection with client
-def socket_accept():
-    conn, address = s.accept()
-    print("Connection has been established | " + "IP" + address[0] + " | Port " + str(address[1]))
-    send_commands(conn)
-    conn.close()
+def accept_connections():
+    for c in all_connections:
+        c.close()
+    del all_connections[:]
+    del all_addresses[:]
+    while 1:
+        try:
+            conn, address = s.accept()
+            conn.setblocking(1)
+            all_connections.append(conn)
+            all_addresses.append(address)
+            print("\nConnection has been established: " + address[0])
+        except:
+            print("Error accepting connections")
 
-def send_commands(conn):
-    while True:
-        cmd = input()
-        if cmd == 'quit':
-            conn.close()
-            s.close()
-            sys.exit()
-        elif len(str.encode(cmd)) > 0:
-            conn.send(str.encode(cmd))
-            client_response = str(conn.recv(1024), "utf-8")
-            print(client_response, end="")
-
-def main():
-    socket_create()
-    socket_bind()
-    socket_accept()
 
 if __name__ == "__main__":
     main()
